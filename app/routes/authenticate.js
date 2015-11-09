de1f7ff4a5a88d852aee64366689e007378ff800
SinghKaranbir <karanbir@karanbirs-MacBook-Pro.local>
2015-11-09 00:44:00 -0800

    Integrated MongoDB

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -1,40 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+module.exports = function(passport){
 
-module.exports  = function(passport){
+    //sends successful login state back to angular
+    router.get('/success', function(req, res){
+        res.send({state: 'success', user: req.user ? req.user : null});
+    });
 
-	//sends successful login state back to angular
-	router.get('/success', function(req, res){
-		res.send({state: 'success', user: req.user ? res.user : null});
-	});
+    //sends failure login state back to angular
+    router.get('/failure', function(req, res){
+        res.send({state: 'failure', user: null, message: "Invalid username or password"});
+    });
 
-	//sends failure login state back to angular
-	router.get("/failure", function(req, res){
-		res.send({state: 'failure', user: null, message: "Invalid username or password"});
-	});
+    //log in
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
 
-	//Login 
-	router.post('/login', passport.authenticate('login',{
-		successRedirect: '/auth/success',
-		failureRedirect: '/auth/failure'
-	}));
+    //sign up
+    router.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
 
-	 //Signup
-	router.post('/signup', passport.authenticate('signup',{
-		successRedirect: '/auth/success',
-		failureRedirect: '/auth/failure'
-	}));
+    //log out
+    router.get('/signout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
-	//Logout
-	router.get('/signout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+    return router;
 
-	return router;
 }
-
-
-
-
