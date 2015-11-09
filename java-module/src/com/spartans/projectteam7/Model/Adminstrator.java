787e7a249674fd550145f4a54298be3a024ad01d
SinghKaranbir <karanbir@karanbirs-MacBook-Pro.local>
2015-11-08 18:08:18 -0800

    Refreshing Everthing
    Cleaning our repo

--- java-module/src/com/spartans/projectteam7/Model/Adminstrator.java
+++
@@ -1,7 +0,0 @@
-package com.spartans.projectteam7.Model;
-
-/**
- * Created by karanbir on 04/11/15.
- */
-public class Adminstrator extends People {
-}
--- java-module/src/com/spartans/projectteam7/Model/User.java
+++
@@ -1,8 +0,0 @@
-package com.spartans.projectteam7.Model;
-
-/**
- * Created by karanbir on 04/11/15.
- */
-public class User extends People {
-
-}
